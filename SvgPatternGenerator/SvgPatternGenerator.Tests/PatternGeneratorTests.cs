334265b05fba3a266f1d7e3ae3351a0521e5178c
John Kerr <kerr.john@gmail.com>
2016-03-12 01:20:02 +0000

    Initial commit

---
+++ SvgPatternGenerator/SvgPatternGenerator.Tests/PatternGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SvgPatternGenerator.Tests
+{
+	[TestFixture]
+	public class PatternGeneratorTests
+	{
+		private readonly string _redAndWhiteStripes = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPSc1MCcgaGVpZ2h0PSc1MCc+DQoJPHJlY3Qgd2lkdGg9JzUwJyBoZWlnaHQ9JzUwJyBmaWxsPSdyZWQnIC8+DQoJPHJlY3QgeD0nMjUnIHdpZHRoPScyNScgaGVpZ2h0PSc1MCcgZmlsbD0nd2hpdGUnIC8+DQo8L3N2Zz4=";
+
+		[Test]
+		public void Can_get_vertical_stripes()
+		{
+			//act
+			var redAndWhiteStripes = PatternGenerator.VerticalStripes("red", "white");
+
+			//assert
+			Assert.That(redAndWhiteStripes, Is.EqualTo(_redAndWhiteStripes));
+		}
+	}
+}
---
+++ SvgPatternGenerator/SvgPatternGenerator/PatternGenerator.cs
@@ -0,0 +1,17 @@
+﻿using SvgPatternGenerator.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgPatternGenerator
+{
+    public static class PatternGenerator
+    {
+		public static string VerticalStripes(string primaryColour, string secondaryColour)
+		{
+			return new VerticalStripes(primaryColour, secondaryColour).Base64Image;
+		}
+    }
+}
